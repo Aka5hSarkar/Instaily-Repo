b89fbc369f3542d4763d85cbad039d7274986c38
Aka5hSarkar <98515020+Aka5hSarkar@users.noreply.github.com>
2023-03-09 12:43:19 +0530

    Delete database_config.php

--- database_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-    function connect(){
-        $connection=mysqli_connect('localhost','root','root','mydb');
-        if(!$connection){
-            die('error');
-        }
-        return $connection;
-    }
-?>
